bc9be62c8c4c32f88379158385311a98cef84568
galbaneim <galbane@post.bgu.ac.il>
2020-04-22 15:17:02 +0300

    updated the byte arr func

--- src/algorithms/mazeGenerators/Maze.java
+++ src/algorithms/mazeGenerators/Maze.java
@@ -1,7 +1,9 @@
 package algorithms.mazeGenerators;
 
 //import javax.swing.text.Position;
-//import java.util.Arrays;
+import java.util.Arrays;
+import java.nio.ByteBuffer;
+
 
 public class Maze {
 
@@ -21,6 +23,12 @@ public class Maze {
         this.exit = exit;
         //this.characters = characters;
         this.data = data;
+    }
+
+    public Maze(byte [] arr )
+
+    {
+       //int x = ByteBuffer.wrap(bytes).getInt();
     }
 
 
@@ -111,12 +119,34 @@ public class Maze {
 
 
     public byte[] toByteArray() {
+
         int rowSize = data.length;
         int colSize = data[0].length;
-        byte [] byteArr = new byte[rowSize * colSize + 16];//16 for the entry, exit, size of row, size of col
-        int byteCounter = 16;
+        byte [] byteArr = new byte[rowSize * colSize + 24]; // 24 for the entry, exit, size of row, size of col
+        int byteCounter = 24;
 
-        //TODO: add the first 16 bytes!
+        //TODO: add the first 24 bytes!
+
+        // first we initial the size of the maze in the four bits and set those values into  values in the arr bit
+        byte [] RowSizeBytes = ByteBuffer.allocate(4).putInt(rowSize).array();
+        byte [] ColSizeBytes = ByteBuffer.allocate(4).putInt(colSize).array();
+
+        // first we initial the start and end position of the maze in the four bits and set those values into  four values in the arr bit
+
+        byte [] StartRowBytes = ByteBuffer.allocate(4).putInt(this.entry.getRowIndex()).array();
+        byte [] StartColBytes = ByteBuffer.allocate(4).putInt(this.entry.getColumnIndex()).array();
+
+        byte [] EndRowBytes = ByteBuffer.allocate(4).putInt(this.exit.getRowIndex()).array();
+        byte [] EndColBytes = ByteBuffer.allocate(4).putInt(this.exit.getColumnIndex()).array();
+
+        //
+        System.arraycopy(RowSizeBytes, 0, byteArr, 0, 4);
+        System.arraycopy(ColSizeBytes, 0, byteArr, 4, 4);
+        System.arraycopy(StartRowBytes, 0, byteArr, 8, 4);
+        System.arraycopy(StartColBytes, 0, byteArr, 12, 4);
+        System.arraycopy(EndRowBytes, 0, byteArr, 16, 4);
+        System.arraycopy(EndColBytes, 0, byteArr, 20, 4);
+
 
         for (int i = 0; i < data.length; i++)
         {
